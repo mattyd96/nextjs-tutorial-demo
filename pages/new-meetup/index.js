7c576b0a6e04e06829219839b8719126f87c97b4
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-02 00:15:36 +1030

    fixed a react hook error in new-meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,9 +2,9 @@ import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
-    const router = useRouter();
 
     const response = await fetch('/api/new-meetup', {
       method: 'POST',
